fc325c05d0ce362e12fd1776cde430ca0bcb347c
leenh bustmante <hackingleenh@gmail.com>
2022-10-20 21:51:25 -0500

    formulario para crear post

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,13 +7,89 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    <div class="card">
+        <div class="card-body">
+            {!! Form::open(['route' => 'admin.posts.store']) !!}
+            <div class="form-group">
+                {!! Form::label('name', 'Nombre') !!}
+                {!! Form::text('name', null, [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ingrese nombre del Post',
+                    'autocomplete' => 'off',
+                ]) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('slug', 'Slug') !!}
+                {!! Form::text('slug', null, [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ingrese el slugo del Post',
+                    'readonly',
+                ]) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('idcategory', 'Categoria') !!}
+                {!! Form::select('idcategory', $categories, null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                <p class="font-weight-bold">Etiquetas:</p>
+                @foreach ($tags as $tag)
+                    <label class="mr-2">
+                        {!! Form::checkbox('tags[]', $tag->id, null) !!}
+                        {{ $tag->tag_name }}
+                    </label>
+                @endforeach
+            </div>
+            <div class="form-group">
+                <p class="font-weight-bold">Estado</p>
+                <label class="mr-2">
+                    {!! Form::radio('status', 1, true) !!}
+                    Borrador
+                </label>
+                <label>
+                    {!! Form::radio('status', 2) !!}
+                    Publicar
+                </label>
+            </div>
+            <div class="form-group">
+                {!! Form::label('extract', 'Extracto') !!}
+                {!! Form::textarea('extract', null, [
+                    'class' => 'form-control',
+                    'rows' => '2',
+                ]) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('body', 'Contenido del Post') !!}
+                {!! Form::textarea('body', null, [
+                    'class' => 'form-control',
+                    // 'rows' => '2',
+                ]) !!}
+            </div>
+            {!! Form::submit('Crear Post', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    <script src="{{ asset('/vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js') }}"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/35.2.0/classic/ckeditor.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            });
+            ClassicEditor
+                .create(document.querySelector('#extract'))
+                .catch(error => {
+                    console.error(error);
+                });
+            ClassicEditor
+                .create(document.querySelector('#body'))
+                .catch(error => {
+                    console.error(error);
+                });
+        });
+    </script>
+@endsection
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
+    <a class="btn btn-sm btn-secondary float-right" href="{{ route('admin.posts.create') }}">Nuevo Post</a>
     <h1>Listado de Post</h1>
 @stop
 
